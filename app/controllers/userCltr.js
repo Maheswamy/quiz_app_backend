bd177201f5403b058d8d952f1cfc536479b01b0b
Maheswamy <mahendragowdas1997@gmail.com>
2023-10-24 20:13:26 +0530

    status code added

--- app/controllers/userCltr.js
+++ app/controllers/userCltr.js
@@ -11,7 +11,7 @@ usersCltr.register = async (req, res) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
     const firstUser = await User.count();
 
